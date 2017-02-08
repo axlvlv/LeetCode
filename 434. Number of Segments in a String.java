8188a3c09054a403f4335af122cadaab84ce8d8e
axlvlv <anniexueshanlu@gmail.com>
2017-02-08 15:49:59 -0500

    Create 434. Number of Segments in a String.java

---
+++ 434. Number of Segments in a String.java
@@ -0,0 +1,27 @@
+/*
+Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+Please note that the string does not contain any non-printable characters.
+
+Example:
+
+Input: "Hello, my name is John"
+Output: 5
+/*
+
+/*
+Method
+Take the word is a whole part 
+/*
+
+public class Solution {
+    public int countSegments(String s) {
+        int res = 0;
+        for(int i = 0; i < s.length(); i++){
+            if((s.charAt(i) != ' ') && (i == 0 || s.charAt(i - 1) == ' ')){
+                res ++;
+            }
+        }
+        return res;
+    }
+}
