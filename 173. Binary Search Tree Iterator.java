3e22c5dce30c912a1434e0755335d8d0e47af507
axlvlv <anniexueshanlu@gmail.com>
2017-02-11 10:55:36 -0500

    Create 173. Binary Search Tree Iterator.java

---
+++ 173. Binary Search Tree Iterator.java
@@ -0,0 +1,22 @@
+/*
+Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+
+Calling next() will return the next smallest number in the BST.
+
+Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+/*
+
+/*
+Method
+
+/*
+
+/*
+Basic Java
+二叉查找树（英语：Binary Search Tree），也称二叉搜索树、有序二叉树（英语：ordered binary tree），排序二叉树（英语：sorted binary tree），是指一棵空树或者具有下列性质的二叉树：
+若任意节点的左子树不空，则左子树上所有结点的值均小于它的根结点的值；
+若任意节点的右子树不空，则右子树上所有结点的值均大于它的根结点的值；
+任意节点的左、右子树也分别为二叉查找树；
+没有键值相等的节点。
+二叉查找树相比于其他数据结构的优势在于查找、插入的时间复杂度较低。为O(log n)。
+
