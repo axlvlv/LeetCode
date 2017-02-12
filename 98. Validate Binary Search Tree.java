6f8476ef11fe86770e9a8d71345f77235c70a86c
axlvlv <anniexueshanlu@gmail.com>
2017-02-12 14:31:36 -0500

    Update 98. Validate Binary Search Tree.java

--- 98. Validate Binary Search Tree.java
+++ 98. Validate Binary Search Tree.java
@@ -35,3 +35,31 @@ public class Solution {
         return helper(root.left, minValue, root.val) && helper(root.right, root.val, maxValue);
     }
 }
+
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+  
+        while(root != null || !stack.isEmpty()){
+            while(root != null){
+            stack.add(root);
+            root = root.left;
+            }
+            
+            root = stack.pop();
+            if(pre != null && root.val <= pre.val){
+                return false;
+            }
+            pre = root;
+            root = root.right;
+        }
+        
+        return true;
+        
+    }
+}
