6fc445910aba46b7981b106d58cf1451ffdcb9fe
axlvlv <anniexueshanlu@gmail.com>
2017-02-24 13:33:39 -0500

    Update 264. Ugly Number II.java

--- 264. Ugly Number II.java
+++ 264. Ugly Number II.java
@@ -6,6 +6,14 @@ Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For 
 Note that 1 is typically treated as an ugly number, and n does not exceed 1690.
 /*
 
+(1) 1×2, 2×2, 3×2, 4×2, 5×2, …
+(2) 1×3, 2×3, 3×3, 4×3, 5×3, …
+(3) 1×5, 2×5, 3×5, 4×5, 5×5, …
+
+仔细观察上述三个列表，我们可以发现每个子列表都是一个丑陋数分别乘以2,3,5，而要求的丑陋数就是从已经生成的序列中取出来的，
+我们每次都从三个列表中取出当前最小的那个加入序列
+
+
 public class Solution {
     public int nthUglyNumber(int n) {
         int[] ugly = new int[1691];
