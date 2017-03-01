19bf7e11c18df223b40498a8671eb5f344d8930b
axlvlv <anniexueshanlu@gmail.com>
2017-03-01 10:16:55 -0500

    Update 206. Reverse Linked List.java

--- 206. Reverse Linked List.java
+++ 206. Reverse Linked List.java
@@ -1,6 +1,8 @@
 /*
 Reverse a singly linked list.
-/*
+*/
+
+
 
 public class Solution {
     public ListNode reverseList(ListNode head) {
@@ -17,5 +19,21 @@ public class Solution {
         }
         
         return pre;
+    }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverse(head);
+    }
+    
+    private ListNode reverse(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
 }
