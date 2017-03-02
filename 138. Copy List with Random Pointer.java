c4b8029dc7f0c37d45f3aee64c2df9cb5b3cef3b
axlvlv <anniexueshanlu@gmail.com>
2017-03-02 16:42:55 -0500

    Update 138. Copy List with Random Pointer.java

--- 138. Copy List with Random Pointer.java
+++ 138. Copy List with Random Pointer.java
@@ -21,5 +21,39 @@ Iterate the original list and duplicate each node. The duplicate
 of each node follows its original immediately.
 Iterate the new list and assign the random pointer for each duplicated node.
 Restore the original list and extract the duplicated nodes.
-/*
+*/
+
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if(head == null){
+            return head;
+        }
+        Map<RandomListNode,RandomListNode> map = new HashMap<RandomListNode,RandomListNode>();
+        RandomListNode newHead = new RandomListNode(head.label);
+        RandomListNode p = head;
+        RandomListNode q = newHead;
+        map.put(head, newHead);
+        p = p.next;
+        while(p != null){
+            RandomListNode temp = new RandomListNode(p.label);
+            map.put(p, temp);
+            q.next = temp;
+            q = temp;
+            p = p.next;
+        }
+        
+        p = head;
+        q = newHead;
+        while(p != null){
+            if(p.random != null){
+                q.random = map.get(p.random);
+            }else{
+                q.random = null;
+            }
+            p = p.next;
+            q = q.next;
+        }
+        return newHead;
+    }
+}
 
