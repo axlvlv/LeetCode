13138d70ad52d9951e024135b40d7b413619af58
axlvlv <anniexueshanlu@gmail.com>
2017-03-14 16:35:10 -0400

    Update 35. Search Insert Position.java

--- 35. Search Insert Position.java
+++ 35. Search Insert Position.java
@@ -26,38 +26,27 @@ public class Solution {
         if(nums == null || nums.length == 0){
             return 0;
         }
-        
-        int start = 0, end = nums.length - 1;
-        
-        while(start + 1 < end){
-            int mid = start + (end - start)/2;
-            if(nums[mid] == target){
-                return mid;
-            }else if(nums[mid] < target){
-                start = mid;
-            }else{
-                end = mid;
-            }
-        }
-        
-        if(nums[start] == target){
-            return start;
-        }
-        
-        if(nums[start] > target){
+        int start = 0, end = nums.length - 1, mid;
+        if(target <= nums[0]){
             return 0;
         }
-        
-        if(nums[end] == target){
-            return end;
+        if(target > nums[end]){
+            return nums.length;
         }
-        
-        if(nums[end] < target){
-            return end + 1;
+        while(start + 1 < end){
+            mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                end = mid;
+            }else if(nums[mid] > target){
+                end = mid;
+            }else if(nums[mid] < target){
+                start = mid;
+            }
         }
-        
-        
-        return start + 1;
-        
+        if(nums[start] == target){
+            return start - 1;
+        }else{
+            return start + 1;
+        }
     }
 }
